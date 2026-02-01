c6ef847e1ef4dccdd609570b4dc34dfcec8305c2
aritazaman <zaman.arita@gmail.com>
2026-01-31 23:44:04 -0500

    audio alert functionality

--- posture-app/script.js
+++ posture-app/script.js
@@ -1,38 +1,65 @@
 const statusDiv = document.getElementById("status");
-const angleText = document.getElementById("angle");
+// const angleText = document.getElementById("angle");
+
+let lastPosture = "good";
+let lastAlertTime = 0; 
+
+
+const alertSound = document.getElementById("chimeAudio");
+const enableBtn = document.getElementById("enableSound");
+
+let soundEnabled = false;
+
+enableBtn.addEventListener("click", () => {
+  alertSound.play().then(() => {
+
+    alertSound.pause();
+    alertSound.currentTime = 0;
+    soundEnabled = true;
+    enableBtn.style.display = "none";
+  });
+});
+
 
 statusDiv.textContent = "Waiting for posture data...";
 statusDiv.className = "waiting";
 
 const socket = new WebSocket("ws://localhost:8080");
 
-// socket.onmessage = function(event) {
-//   const data = event.data;
-
-//   if (data === "good") {
-//     statusDiv.textContent = "Good Posture";
-//     statusDiv.className = "good";
-//   } else if (data === "slouch") {
-//     statusDiv.textContent = "Bad Posture";
-//     statusDiv.className = "bad";
-//   } else {
-//     // If Arduino sends angle numbers later
-//     angleText.textContent = "Angle: " + data;
-//   }
-// };
-
 socket.onmessage = function(event) {
   const data = event.data.trim().toLowerCase(); // trim whitespace and lowercase
-
+  let currentPosture = lastPosture; 
+  
   if (data.includes("good")) {
     statusDiv.textContent = "Good Posture";
     statusDiv.className = "good";
+    currentPosture = "good";
+
   } else if (data.includes("slouch")) {
     statusDiv.textContent = "Fix Your Posture";
     statusDiv.className = "bad";
+    currentPosture = "bad";
+  
+    if (
+      lastPosture === "good" &&
+      soundEnabled &&
+      Date.now() - lastAlertTime > 2000
+    ) {
+      alertSound.currentTime = 0;
+      alertSound.play();
+      lastAlertTime = Date.now();
+    }
+
+
+
   } else {
     // Optional: show angle if Arduino sends a number
-    angleText.textContent = "Angle: " + data;
+    // angleText.text
+    // Content = "Angle: " + data;
   }
+
+  lastPosture = currentPosture;
+
+  
 };
 
